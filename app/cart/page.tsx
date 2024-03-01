cfe17788a295a495e55cedfd5ab0acdc513c4a03
TuanDat2514 <dattuantrinh77@gmail.com>
2024-03-01 12:10:08 +0700

    responsive detailcart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,19 @@
+import {Col, Row} from "antd";
+import './style.scss';
+
 export default function Cart() {
-  return (
-    <div className="pt-52">
-      <p>cart</p>
-    </div>
-  );
+    return (
+        <div className="layout-cart pt-52 ">
+            <div className="wrapper-mainCart">
+                <div className="content-bodyCart">
+                    <div className="container mx-auto px-4">
+                        <Row>
+                            <Col span={14}>14</Col>
+                            <Col span={6}>6</Col>
+                        </Row>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
